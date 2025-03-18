27a17dc9258f8cc1762097145e08a44b7262c706
bareebaree <james_tate@ymail.com>
2025-03-18 12:07:30 +0000

    Delete src directory

--- src/run_blastn.py
+++
@@ -1,5 +0,0 @@
-from Bio.Blast.Applications import NcbiblastnCommandline
-
-blastn_cline = NcbiblastnCommandline(query="query.fasta", db="mydb",
-                                     out="blast_results.xml", outfmt=5)
-stdout, stderr = blastn_cline()
